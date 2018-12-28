f6e40bc4fa2f2e3b10a0219acac8fa8cbebc12f3
Rene Ventura <venturar729@gmail.com>
2018-12-28 02:25:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,6 +5,10 @@ def load_library(filename)
   # code goes here
   emoticon_hash = YAML.load_file(filename)
   emoticon_dic = {"get_meaning" => {},  "get_emoticon" => {}}
+  emoticon_hash.each do |key, val|
+    emoticon_dic["get_meaning"] = {val[0] => key}
+    emoticon_dic["get_emoticon"] = {val[1] => key}
+  end
 end
 
 def get_japanese_emoticon
